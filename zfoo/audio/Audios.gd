4f47aa6ad7259de837978b14595884da6e283b2e
jaysunxiao <jaysunxiao@gmail.com>
2026-07-06 23:25:05 +0800

    feat[audio]: add SoundEffect type and play_sfx helper

--- zfoo/audio/Audios.gd
+++ zfoo/audio/Audios.gd
@@ -4,29 +4,29 @@ extends Object
 # Multi-channel audio: a pool of AudioStreamPlayers that can play multiple sounds concurrently.
 
 const COUNT: int = 8
-const BUS_NAME: String = "SoundEffect"
+const SFX_BUS_NAME: String = "SoundEffect"
 
 static var audios: Array[AudioStreamPlayer] = []
 
 static func init() -> void:
 	AudioServer.add_bus()
 	var bus_index := AudioServer.get_bus_count() - 1
-	AudioServer.set_bus_name(bus_index, BUS_NAME)
+	AudioServer.set_bus_name(bus_index, SFX_BUS_NAME)
 
 	var sfx_node := Node.new()
-	sfx_node.name = BUS_NAME
+	sfx_node.name = SFX_BUS_NAME
 	gdf.gdf_node.add_child(sfx_node)
 	for i in range(COUNT):
 		var player := AudioStreamPlayer.new()
 		player.name = StringUtils.format("sfx_{}", i)
-		player.bus = BUS_NAME
+		player.bus = SFX_BUS_NAME
 		player.finished.connect(func() -> void: player.stream = null)
 		sfx_node.add_child(player)
 		audios.append(player)
 	pass
 
 static func set_bus_volume_linear(volume_linear: float) -> void:
-	AudioServer.set_bus_volume_linear(AudioServer.get_bus_index(BUS_NAME), clampf(volume_linear, 0.0, 1.0))
+	AudioServer.set_bus_volume_linear(AudioServer.get_bus_index(SFX_BUS_NAME), clampf(volume_linear, 0.0, 1.0))
 	pass
 
 static func play(path: String, volume_linear: float = 1.0) -> void:
@@ -45,6 +45,10 @@ static func play(path: String, volume_linear: float = 1.0) -> void:
 	player.volume_linear = clampf(volume_linear, 0.0, 1.0)
 	player.stream = resource
 	player.play()
+	pass
+
+static func play_sfx(effect: SoundEffect) -> void:
+	await play(effect.path, effect.volume_linear)
 	pass
 
 static func stop_all() -> void:
