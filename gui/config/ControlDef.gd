53e058878d48e6a3fe5c8a9395174fd165ba704a
jaysunxiao <jaysunxiao@gmail.com>
2026-09-02 08:50:26 +0800

    chore[gui]: add GraphNodeDef interface implementation markers

--- gui/config/ControlDef.gd
+++ gui/config/ControlDef.gd
@@ -3,10 +3,11 @@ extends GraphNodeDef
 
 var id: String = ""
 
-
+# GraphNodeDef-Interface-Implement-Start
 func catalog_id() -> String:
 	return id
 
 
 func is_batch() -> bool:
 	return true
+# GraphNodeDef-Interface-Implement-End
--- gui/config/SkillDef.gd
+++ gui/config/SkillDef.gd
@@ -5,6 +5,7 @@ var skill: String = ""
 var category: String = ""
 var cli: String = ""
 
-
+# GraphNodeDef-Interface-Implement-Start
 func catalog_id() -> String:
 	return skill
+# GraphNodeDef-Interface-Implement-End
