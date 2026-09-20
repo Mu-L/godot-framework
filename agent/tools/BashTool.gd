1f99d8abc0f833da0d258f4731d973a0caf51121
jaysunxiao <jaysunxiao@gmail.com>
2026-09-20 20:34:15 +0800

    fix[agent]: locate git bash via where.exe with cached path

--- agent/tools/BashTool.gd
+++ agent/tools/BashTool.gd
@@ -3,10 +3,6 @@ extends AgentTool
 
 const NAME := "bash"
 const ARG_COMMAND := "command"
-const WINDOWS_GIT_BASH_PATHS: PackedStringArray = [
-	"C:/Program Files/Git/bin/bash.exe",
-	"C:/Program Files (x86)/Git/bin/bash.exe",
-]
 
 
 func _init() -> void:
@@ -42,14 +38,43 @@ func build_argv_from_args(args: Dictionary[String, Variant]) -> PackedStringArra
 		return PackedStringArray()
 	var bash := "/bin/bash"
 	if OSUtils.is_windows():
-		bash = "bash.exe"
-		for path in WINDOWS_GIT_BASH_PATHS:
-			if FileAccess.file_exists(path):
-				bash = path
-				break
+		bash = find_windows_git_bash()
 	var working_directory := AgentWorkspace.get_root()
 	var bash_command := StringUtils.format("cd -- '{}' && {}", working_directory, command)
 	if OSUtils.is_windows():
 		var encoded_command := Marshalls.raw_to_base64(command.to_utf8_buffer())
 		bash_command = StringUtils.format("cd -- '{}' && /bin/bash --noprofile --norc <(printf %s {} | /usr/bin/base64 --decode)", working_directory, encoded_command)
 	return PackedStringArray([bash, "--noprofile", "--norc", "-c", bash_command])
+
+# ----------------------------------------------------------------------------------------------------------------------
+static var windows_git_bash_path := ""
+
+static func find_windows_git_bash() -> String:
+	if StringUtils.is_not_blank(windows_git_bash_path):
+		return windows_git_bash_path
+
+	var where_result := OSUtils.execute(PackedStringArray(["where.exe", "git"]), false)
+	if where_result.exit_code == 0:
+		var output := FileUtils.normalize_line_endings_to_lf(where_result.output.build_string())
+		for line in output.split("\n", false):
+			var git_path := line.strip_edges().replace("\\", "/")
+			var git_directory := git_path.get_base_dir()
+			var candidates: PackedStringArray = [
+				git_directory.path_join("bash.exe"),
+				git_directory.get_base_dir().path_join("bin/bash.exe"),
+			]
+			for candidate in candidates:
+				if FileAccess.file_exists(candidate):
+					windows_git_bash_path = candidate
+					return windows_git_bash_path
+
+	const other_paths: PackedStringArray = [
+		"C:/Program Files/Git/bin/bash.exe",
+		"C:/Program Files (x86)/Git/bin/bash.exe",
+		]
+	for path in other_paths:
+		if FileAccess.file_exists(path):
+			windows_git_bash_path = path
+			return windows_git_bash_path
+	windows_git_bash_path = "bash.exe"
+	return windows_git_bash_path
