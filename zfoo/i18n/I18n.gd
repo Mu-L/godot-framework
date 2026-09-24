a259504755963477f52ed97d538d81a369161728
jaysunxiao <jaysunxiao@gmail.com>
2026-09-24 21:45:08 +0800

    feat[i18n]: persist selected locale in settings

--- zfoo/i18n/I18n.gd
+++ zfoo/i18n/I18n.gd
@@ -1,6 +1,8 @@
 class_name I18n
 extends Object
 
+const LOCALE_SETTING_KEY := "locale"
+
 
 ## Loads the translation at path when needed, then switches to its declared locale.
 static func set_locale(locale_path: String) -> void:
@@ -10,6 +12,9 @@ static func set_locale(locale_path: String) -> void:
 	if not TranslationServer.has_translation_for_locale(locale_data.locale, true):
 		TranslationServer.add_translation(create_translation(locale_data))
 	TranslationServer.set_locale(locale_data.locale)
+	
+	Setting.set_string(LOCALE_SETTING_KEY, locale_data.locale)
+	Setting.save()
 	gdf.events.locale_changed.emit()
 	pass
 
