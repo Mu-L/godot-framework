f9828217548d9a2cbb665619e5cbedcd9dfa6e33
jaysunxiao <jaysunxiao@gmail.com>
2026-09-24 21:49:28 +0800

    fix[i18n]: remove stale translation on locale switch

--- zfoo/i18n/I18n.gd
+++ zfoo/i18n/I18n.gd
@@ -13,13 +13,24 @@ static func set_locale(locale_path: String) -> void:
 	var locale_data := LocaleData.parse_json_file(locale_path)
 	if locale_data == null:
 		return
+	remove_old_locale()
 	if not TranslationServer.has_translation_for_locale(locale_data.locale, true):
 		TranslationServer.add_translation(create_translation(locale_data))
 	TranslationServer.set_locale(locale_data.locale)
-	
 	Setting.set_string(LOCALE_SETTING_KEY, locale_data.locale)
 	Setting.save()
 	gdf.events.locale_changed.emit()
+	pass
+
+
+static func remove_old_locale() -> void:
+	var old_locale := get_locale()
+	if old_locale.is_empty():
+		return
+	var standardized_locale := TranslationServer.standardize_locale(old_locale)
+	for translation: Translation in TranslationServer.get_translations():
+		if TranslationServer.standardize_locale(translation.locale) == standardized_locale:
+			TranslationServer.remove_translation(translation)
 	pass
 
 
