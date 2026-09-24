53b3c854997073db19b4afffa32d4c394a98e784
jaysunxiao <jaysunxiao@gmail.com>
2026-09-24 09:13:43 +0800

    feat[style]: add Margin spacing scale constants

---
+++ zfoo/style/Margin.gd
@@ -0,0 +1,30 @@
+class_name Margin
+extends Object
+
+## Spacing helpers for modifying the padding and margin of an element.
+##
+## Values follow a 4px step scale, e.g. [code]Margin.ma_4[/code] == 16.
+## Typical use:
+## [codeblock]
+## var margin := MarginContainer.new()
+## margin.add_theme_constant_override("margin_left", Margin.ma_4)
+## margin.add_theme_constant_override("margin_right", Margin.ma_4)
+## [/codeblock]
+
+const ma_0 := 0
+const ma_1 := 4
+const ma_2 := 8
+const ma_3 := 12
+const ma_4 := 16
+const ma_5 := 20
+const ma_6 := 24
+const ma_7 := 28
+const ma_8 := 32
+const ma_9 := 36
+const ma_10 := 40
+const ma_11 := 44
+const ma_12 := 48
+const ma_13 := 52
+const ma_14 := 56
+const ma_15 := 60
+const ma_16 := 64
